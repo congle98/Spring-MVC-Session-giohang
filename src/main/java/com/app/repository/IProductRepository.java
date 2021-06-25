0e19816ae72abb509eff50bcf55856753e1bc768
congle98 <79907265+congle98@users.noreply.github.com>
2021-06-25 11:18:42 +0700

    Spring-MVC-session-giohang

---
+++ src/main/java/com/app/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.app.repository;
+
+import com.app.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
+}
